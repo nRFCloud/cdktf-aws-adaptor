4a666c425c2d8b38672b9929c8b18e101d1b71fd
semcc <sean.mccullough@nordicsemi.no>
2023-12-11 10:06:07 -0800

    Revert "refactor: debug log"
    This reverts commit 65f41bbb9b20760c92d9bc868f6e3ac732033559.

--- src/mappings/services/sqs.ts
+++ src/mappings/services/sqs.ts
@@ -6,8 +6,6 @@ import { registerMappingTyped } from "../utils.js";
 export function registerSqsMappings() {
   registerMappingTyped(CfnQueuePolicy, SqsQueuePolicy, {
     resource(scope, id, props) {
-      console.log(id);
-      console.log(props);
       const policiesEncoded = Fn.jsonencode(props.PolicyDocument);
       const sqsQueuePolicies = props.Queues.map((queue): SqsQueuePolicyConfig => ({
         policy: policiesEncoded,
@@ -22,7 +20,7 @@ export function registerSqsMappings() {
           sqsQueuePolicy.node.addDependency(prevConfig);
         }
       }
-      console.log(sqsQueuePolicies);
+
       return sqsQueuePolicies.at(-1)!;
     },
     attributes: {
