70c496f153bdc095a9020d26cd5f015d4d4f0a2e
semcc <sean.mccullough@nordicsemi.no>
2024-02-05 17:26:05 -0800

    wip

--- src/mappings/services/s3.ts
+++ src/mappings/services/s3.ts
@@ -1,5 +1,6 @@
 import { S3BucketPolicy } from "@cdktf/provider-aws/lib/s3-bucket-policy/index.js";
-import { CfnBucketPolicy } from "aws-cdk-lib/aws-s3";
+import { S3Bucket } from "@cdktf/provider-aws/lib/s3-bucket/index.js";
+import { CfnBucket, CfnBucketPolicy } from "aws-cdk-lib/aws-s3";
 import { Fn } from "cdktf";
 import { deleteUndefinedKeys, registerMappingTyped } from "../utils.js";
 
@@ -17,6 +18,31 @@ export function registerS3Mappings() {
     },
     attributes: {
       Ref: (resource) => resource.id,
+    },
+  });
+
+  registerMappingTyped(CfnBucket, S3Bucket, {
+    resource(scope, id, props) {
+      return new S3Bucket(
+        scope,
+        id,
+        deleteUndefinedKeys({
+          accelerationStatus: props?.AccelerateConfiguration?.AccelerationStatus,
+          acl: props?.AccessControl,
+          bucket: props?.BucketName,
+          bucketPrefix: id,
+        }),
+      );
+    },
+
+    attributes: {
+      Ref: (resource) => resource.id,
+      Arn: (resource) => resource.arn,
+      DomainName: (resource) => resource.bucketDomainName,
+      RegionalDomainName: (resource) => resource.bucketRegionalDomainName,
+      WebsiteUrl: (resource) => resource.websiteDomain,
+      DualStackDomainName: (resource) =>
+        `${resource.bucket}.s3.dualstack.${resource.region.toLocaleLowerCase()}.amazonaws.com`,
     },
   });
 }
