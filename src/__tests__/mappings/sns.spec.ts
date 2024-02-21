2396aa75cc74e853018f82a32f3383f8c8254ce6
John Conley <8932043+jfrconley@users.noreply.github.com>
2024-02-21 14:31:41 -0800

    add explicit topic mapping

--- src/__tests__/mappings/sns.spec.ts
+++ src/__tests__/mappings/sns.spec.ts
@@ -1,5 +1,7 @@
+import { SnsTopicDataProtectionPolicy } from "@cdktf/provider-aws/lib/sns-topic-data-protection-policy/index.js";
 import { SnsTopicSubscription } from "@cdktf/provider-aws/lib/sns-topic-subscription/index.js";
-import { CfnSubscription } from "aws-cdk-lib/aws-sns";
+import { SnsTopic } from "@cdktf/provider-aws/lib/sns-topic/index.js";
+import { CfnSubscription, CfnTopic } from "aws-cdk-lib/aws-sns";
 import { setupJest } from "cdktf/lib/testing/adapters/jest.js";
 import { synthesizeElementAndTestStability } from "../helpers.js";
 
@@ -36,6 +38,97 @@ describe("SNS mappings", () => {
           replayPolicy: "${jsonencode({\"replayPolicy\" = true})}",
         },
       );
+    });
+  });
+
+  describe("AWS:SNS:Topic", () => {
+    it("Should translate", () => {
+      const { synth } = synthesizeElementAndTestStability(
+        CfnTopic,
+        {
+          topicName: "ExampleTopic",
+          archivePolicy: { archivePolicy: true },
+          dataProtectionPolicy: { dataProtectionPolicy: true },
+          deliveryStatusLogging: [{
+            protocol: "https",
+            failureFeedbackRoleArn: "FailureFeedbackRoleArnHttps",
+            successFeedbackRoleArn: "SuccessFeedbackRoleArnHttps",
+            successFeedbackSampleRate: "1",
+          }, {
+            protocol: "sqs",
+            failureFeedbackRoleArn: "FailureFeedbackRoleArnsqs",
+            successFeedbackRoleArn: "SuccessFeedbackRoleArnsqs",
+            successFeedbackSampleRate: "2",
+          }, {
+            protocol: "lambda",
+            failureFeedbackRoleArn: "FailureFeedbackRoleArnlambda",
+            successFeedbackRoleArn: "SuccessFeedbackRoleArnlambda",
+            successFeedbackSampleRate: "3",
+          }, {
+            protocol: "application",
+            failureFeedbackRoleArn: "FailureFeedbackRoleArnapplication",
+            successFeedbackRoleArn: "SuccessFeedbackRoleArnapplication",
+            successFeedbackSampleRate: "4",
+          }, {
+            protocol: "firehose",
+            failureFeedbackRoleArn: "FailureFeedbackRoleArnfirehose",
+            successFeedbackRoleArn: "SuccessFeedbackRoleArnfirehose",
+            successFeedbackSampleRate: "5",
+          }],
+          displayName: "ExampleDisplayName",
+          fifoTopic: true,
+          tags: [{
+            key: "ExampleKey",
+            value: "ExampleValue",
+          }],
+          subscription: [{
+            protocol: "https",
+            endpoint: "example-endpoint.example",
+          }],
+          kmsMasterKeyId: "ExampleKmsMasterKeyId",
+          signatureVersion: "1",
+          tracingConfig: "PASSTHROUGH",
+          contentBasedDeduplication: true,
+        },
+        SnsTopic,
+        {
+          name: "ExampleTopic",
+          archivePolicy: "${jsonencode({\"archivePolicy\" = true})}",
+          applicationFailureFeedbackRoleArn: "FailureFeedbackRoleArnapplication",
+          applicationSuccessFeedbackRoleArn: "SuccessFeedbackRoleArnapplication",
+          applicationSuccessFeedbackSampleRate: 4,
+          firehoseFailureFeedbackRoleArn: "FailureFeedbackRoleArnfirehose",
+          firehoseSuccessFeedbackRoleArn: "SuccessFeedbackRoleArnfirehose",
+          firehoseSuccessFeedbackSampleRate: 5,
+          httpSuccessFeedbackSampleRate: 1,
+          httpFailureFeedbackRoleArn: "FailureFeedbackRoleArnHttps",
+          httpSuccessFeedbackRoleArn: "SuccessFeedbackRoleArnHttps",
+          lambdaSuccessFeedbackSampleRate: 3,
+          lambdaFailureFeedbackRoleArn: "FailureFeedbackRoleArnlambda",
+          lambdaSuccessFeedbackRoleArn: "SuccessFeedbackRoleArnlambda",
+          sqsSuccessFeedbackSampleRate: 2,
+          sqsFailureFeedbackRoleArn: "FailureFeedbackRoleArnsqs",
+          sqsSuccessFeedbackRoleArn: "SuccessFeedbackRoleArnsqs",
+          displayName: "ExampleDisplayName",
+          fifoTopic: true,
+          tags: { ExampleKey: "ExampleValue" },
+          kmsMasterKeyId: "ExampleKmsMasterKeyId",
+          signatureVersion: 1,
+          tracingConfig: "PASSTHROUGH",
+          contentBasedDeduplication: true,
+        },
+      );
+
+      expect(synth).toHaveResourceWithProperties(SnsTopicDataProtectionPolicy, {
+        "arn": "${aws_sns_topic.resource_22C949BF.arn}",
+        "policy": "${jsonencode({\"dataProtectionPolicy\" = true})}",
+      });
+
+      expect(synth).toHaveResourceWithProperties(SnsTopicSubscription, {
+        "endpoint": "example-endpoint.example",
+        "protocol": "https",
+        "topic_arn": "${aws_sns_topic.resource_22C949BF.arn}",
+      });
     });
   });
 });
--- src/mappings/services/sns.ts
+++ src/mappings/services/sns.ts
@@ -1,7 +1,9 @@
+import { SnsTopicDataProtectionPolicy } from "@cdktf/provider-aws/lib/sns-topic-data-protection-policy/index.js";
 import { SnsTopicSubscription } from "@cdktf/provider-aws/lib/sns-topic-subscription/index.js";
-import { CfnSubscription } from "aws-cdk-lib/aws-sns";
+import { SnsTopic, SnsTopicConfig } from "@cdktf/provider-aws/lib/sns-topic/index.js";
+import { CfnSubscription, CfnTopic } from "aws-cdk-lib/aws-sns";
 import { Fn } from "cdktf";
-import { createGenericCCApiMapping, deleteUndefinedKeys, registerMapping, registerMappingTyped } from "../utils.js";
+import { deleteUndefinedKeys, registerMappingTyped } from "../utils.js";
 
 export function registerSnsMappings() {
   registerMappingTyped(CfnSubscription, SnsTopicSubscription, {
@@ -30,5 +32,121 @@ export function registerSnsMappings() {
     },
   });
 
-  registerMapping("AWS::SNS::Topic", createGenericCCApiMapping("AWS::SNS::Topic"));
+  registerMappingTyped(CfnTopic, SnsTopic, {
+    resource(scope, id, props) {
+      let topicConfig: SnsTopicConfig = {
+        name: props?.TopicName,
+        displayName: props?.DisplayName,
+        kmsMasterKeyId: props?.KmsMasterKeyId,
+        archivePolicy: props?.ArchivePolicy && Fn.jsonencode(props.ArchivePolicy),
+        fifoTopic: props?.FifoTopic,
+        contentBasedDeduplication: props?.ContentBasedDeduplication,
+        tracingConfig: props?.TracingConfig,
+        tags: Object.fromEntries(
+          props?.Tags?.map(({
+            Key,
+            Value,
+          }) => [Key, Value]) || [],
+        ),
+        signatureVersion: props?.SignatureVersion ? +props.SignatureVersion : undefined,
+      };
+
+      const httpDeliveryLogging = props?.DeliveryStatusLogging?.find(item =>
+        item.Protocol === "https" || item.Protocol === "http"
+      );
+      if (httpDeliveryLogging) {
+        topicConfig = {
+          ...topicConfig,
+          httpFailureFeedbackRoleArn: httpDeliveryLogging.FailureFeedbackRoleArn,
+          httpSuccessFeedbackRoleArn: httpDeliveryLogging.SuccessFeedbackRoleArn,
+          httpSuccessFeedbackSampleRate: httpDeliveryLogging.SuccessFeedbackSampleRate
+            ? +httpDeliveryLogging.SuccessFeedbackSampleRate
+            : undefined,
+        };
+      }
+
+      const sqsDeliveryLogging = props?.DeliveryStatusLogging?.find(item => item.Protocol === "sqs");
+      if (sqsDeliveryLogging) {
+        topicConfig = {
+          ...topicConfig,
+          sqsFailureFeedbackRoleArn: sqsDeliveryLogging.FailureFeedbackRoleArn,
+          sqsSuccessFeedbackRoleArn: sqsDeliveryLogging.SuccessFeedbackRoleArn,
+          sqsSuccessFeedbackSampleRate: sqsDeliveryLogging.SuccessFeedbackSampleRate
+            ? +sqsDeliveryLogging.SuccessFeedbackSampleRate
+            : undefined,
+        };
+      }
+
+      const lambdaDeliveryLogging = props?.DeliveryStatusLogging?.find(item => item.Protocol === "lambda");
+      if (lambdaDeliveryLogging) {
+        topicConfig = {
+          ...topicConfig,
+          lambdaFailureFeedbackRoleArn: lambdaDeliveryLogging.FailureFeedbackRoleArn,
+          lambdaSuccessFeedbackRoleArn: lambdaDeliveryLogging.SuccessFeedbackRoleArn,
+          lambdaSuccessFeedbackSampleRate: lambdaDeliveryLogging.SuccessFeedbackSampleRate
+            ? +lambdaDeliveryLogging.SuccessFeedbackSampleRate
+            : undefined,
+        };
+      }
+
+      const applicationDeliveryLogging = props?.DeliveryStatusLogging?.find(item => item.Protocol === "application");
+      if (applicationDeliveryLogging) {
+        topicConfig = {
+          ...topicConfig,
+          applicationFailureFeedbackRoleArn: applicationDeliveryLogging.FailureFeedbackRoleArn,
+          applicationSuccessFeedbackRoleArn: applicationDeliveryLogging.SuccessFeedbackRoleArn,
+          applicationSuccessFeedbackSampleRate: applicationDeliveryLogging.SuccessFeedbackSampleRate
+            ? +applicationDeliveryLogging.SuccessFeedbackSampleRate
+            : undefined,
+        };
+      }
+
+      const firehoseDeliveryLogging = props?.DeliveryStatusLogging?.find(item => item.Protocol === "firehose");
+      if (firehoseDeliveryLogging) {
+        topicConfig = {
+          ...topicConfig,
+          firehoseFailureFeedbackRoleArn: firehoseDeliveryLogging.FailureFeedbackRoleArn,
+          firehoseSuccessFeedbackRoleArn: firehoseDeliveryLogging.SuccessFeedbackRoleArn,
+          firehoseSuccessFeedbackSampleRate: firehoseDeliveryLogging.SuccessFeedbackSampleRate
+            ? +firehoseDeliveryLogging.SuccessFeedbackSampleRate
+            : undefined,
+        };
+      }
+
+      // if (props?.DeliveryStatusLogging?.find(item => item.Protocol === "https" || item.Protocol === "http")) {
+      //     topicConfig.httpFailureFeedbackRoleArn
+      // }
+      const topic = new SnsTopic(
+        scope,
+        id,
+        deleteUndefinedKeys(topicConfig),
+      );
+
+      if (props?.DataProtectionPolicy) {
+        new SnsTopicDataProtectionPolicy(scope, `${id}-data-protection-policy`, {
+          arn: topic.arn,
+          policy: Fn.jsonencode(props.DataProtectionPolicy),
+        });
+      }
+
+      for (const subscription of props?.Subscription || []) {
+        new SnsTopicSubscription(
+          topic,
+          `subscription-${subscription.Protocol}-${subscription.Endpoint}`,
+          deleteUndefinedKeys({
+            endpoint: subscription.Endpoint!,
+            protocol: subscription.Protocol,
+            topicArn: topic.arn,
+          }),
+        );
+      }
+
+      return topic;
+    },
+    attributes: {
+      TopicName: (resource) => resource.name,
+      TopicArn: (resource) => resource.arn,
+      Ref: resource => resource.id,
+    },
+  });
 }
